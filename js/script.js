dda7650af56d33871d0e216972c39efce15d3b7e
clement matheron <clement.matheron@gmail.com>
2016-02-08 16:35:57 +0100

    clean up

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 var fiss = ["N6","seth","duch","R1","froute","smidge","popo","kermit","popo","lgg","st","sergz","diggy", "bobol","shirow", "putois","yann","fred","lonewolf", "inu","piston", "dom"]
 
 $(document).ready(function(){
-	$(".one").on('click', function () {
-        $(".one").html(rand(fiss))
+	$("#one").on('click', function () {
+        $("#one > div >  span").html(rand(fiss))
     });
-    $(".two").on('click', function () {
-        $(".two").html(rand(fiss))
+    $("#two").on('click', function () {
+        $("#two > div >  span").html(rand(fiss))
     });
-    $(".three").on('click', function () {
-        $(".three").html(rand(fiss))
+    $("#three").on('click', function () {
+        $("#three > div >  span").html(rand(fiss))
     });
-    $(".four").on('click', function () {
-        $(".four").html(rand(fiss))
+    $("#four").on('click', function () {
+        $("#four > div >  span").html(rand(fiss))
     });
-    $(".five").on('click', function () {
-        $(".five").html(rand(fiss))
+    $("#five").on('click', function () {
+        $("#five > div >  span").html(rand(fiss))
     });
-    $(".six").on('click', function () {
-        $(".six").html(rand(fiss))
+    $("#six").on('click', function () {
+        $("#six > div >  span").html(rand(fiss))
+    });
+    $("#seven").on('click', function () {
+        $("#seven > div >  span").html(rand(fiss))
+    });
+    $("#eight").on('click', function () {
+        $("#eight > div >  span").html(rand(fiss))
+    });
+    $("#nine").on('click', function () {
+        $("#nine > div >  span").html(rand(fiss))
     });
 })
 
